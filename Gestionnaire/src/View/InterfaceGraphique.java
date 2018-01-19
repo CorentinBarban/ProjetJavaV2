400b596e4b83694cebe9afc156a711fe0fd9f69e
maths <maths@DESKTOP-N40H9P1>
2018-01-19 18:40:14 +0100

    Création de la classe InterfaceGraphique + implémentations des méthodes nécessaires afin de créer une interface graphique

---
+++ Gestionnaire/src/View/InterfaceGraphique.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.HeadlessException;
+import javax.swing.*;
+
+/**
+ *
+ * @author maths
+ */
+public class InterfaceGraphique extends JFrame {
+
+    public InterfaceGraphique(String pTitre){
+        super(pTitre);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setContentPane(panneauDeContenu());
+	this.setResizable(false);
+	this.pack();
+    }
+    
+    public JLabel panneauDeContenu() {
+    return null;
+    }
+    
+}
