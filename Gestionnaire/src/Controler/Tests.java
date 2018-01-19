7e0be02861b2265f7448a2ad1aca8fa80412b125
maths <maths@DESKTOP-N40H9P1>
2018-01-19 21:48:22 +0100

    Création d'une classe de tests afin de tester l'implémentation de méthodes vérifiant si les données entrées sont bonnes dans Mission. Je pense les regrouper dans une seule classe qui servira de controleur pour mission. C'est sur, même.

---
+++ Gestionnaire/src/Controler/Tests.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controler;
+
+import Model.*;
+import java.text.ParseException;
+
+/**
+ *
+ * @author maths
+ */
+public class Tests {
+    
+    public static void main (String[] args) throws ParseException{
+        Mission m1 = new Mission("Mission test 1", "06/02/2018", 4);
+    }
+}
--- Gestionnaire/src/Model/Mission.java
+++ Gestionnaire/src/Model/Mission.java
@@ -5,6 +5,8 @@
  */
 package Model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -21,11 +23,11 @@ public class Mission {
     private String[] tabTypes =  {"enPreparation", "plannifiee", "enCours"};
     private String missionType;
     
-    public Mission(String n, Date sd, int md){
+    public Mission(String n, String sd, int md) throws ParseException{
         this.missionName = n;
-        this.startDate = sd;
+        this.startDate = dateCheck(sd);// Appel de la méthode dateCheck qui vérifie si la date est bonne
         this.missionDuration = md;
-        this.missionType = this.tabTypes[0];
+        this.missionType = this.tabTypes[0]; // Lorsqu'une mission est créée, son statut est "en préparation"
     }
     
     public void addSkill(Skill s){
@@ -36,8 +38,23 @@ public class Mission {
         this.personOnMission.put(p.getId(), p);
     }
     
-    public void skillCheck(){
-        // Cette classe doit vérifier si les skills apportés par les personnes présentes sur la mission remplissent les besoins. Si c'est le cas, alors le type de la mission passe à "plannifié".
+    public Date dateCheck(String d) throws ParseException{ // Cette classe doit vérifier si la date de départ assignée lors de la création d'une mission est valide.
+        Date today = Calendar.getInstance().getTime(); // Stockage de la date d'ajd
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); 
+        Date newDate = formatter.parse(d); // Conversion du texte passé en param en date selon le format vu au dessus
+        if(newDate.before(today)){ // Si la date entrée est inférieure à la date d'ajd
+            throw new ParseException("Problème date", 1);
+        } else {
+            return newDate;
+        }
+    }
+    
+    public void skillCheck(){  // Cette classe doit vérifier si les compétences apportées par les personnes présentes sur la mission remplissent les besoins. Si c'est le cas, alors le type de la mission passe à "plannifié".
+        
+    }
+    
+    public ArrayList<String> recommandation(){ // Cette classe permet d'afficher les personnes recommandées par le logiciel pour telle mission.
+        return null;
     }
 
     public String getMissionName() {
