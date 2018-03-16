3bb9531f8b25f5eebbdb761a49b65cb776f8344f
Corentin <corentin.barban@sfr.fr>
2018-03-16 21:18:24 +0100

    Interface Mission

--- GestionnaireV2/src/GUI/NouvelleInterface/MissionList.java
+++ GestionnaireV2/src/GUI/NouvelleInterface/MissionList.java
@@ -27,32 +27,45 @@ public class MissionList extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTableMission = new javax.swing.JTable();
 
         setBackground(new java.awt.Color(242, 242, 242));
 
-        jLabel1.setText("Mission Liste");
+        jTableMission.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTableMission);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(293, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addContainerGap(304, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 636, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(220, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addContainerGap(196, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTableMission;
     // End of variables declaration//GEN-END:variables
 }
---
+++ GestionnaireV2/src/GUI/NouvelleInterface/MissionTableModel.java
@@ -0,0 +1,73 @@
+package GUI.NouvelleInterface;
+
+import API.Mission;
+import API.Mission.Etat;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author corentin
+ */
+public class MissionTableModel extends AbstractTableModel
+{
+    private final List<Mission> missionList;
+     
+    private final String[] columnNames = new String[] {
+            "Id", "Nom", "Date de début","Durée","Avancement"
+    };
+    private final Class[] columnClass = new Class[] {
+        Integer.class, String.class, Etat.class
+    };
+ 
+    public MissionTableModel(List<Mission> missionList)
+    {
+        this.missionList = missionList;
+    }
+     
+    @Override
+    public String getColumnName(int column)
+    {
+        return columnNames[column];
+    }
+ 
+    @Override
+    public Class<?> getColumnClass(int columnIndex)
+    {
+        return columnClass[columnIndex];
+    }
+ 
+    @Override
+    public int getColumnCount()
+    {
+        return columnNames.length;
+    }
+ 
+    @Override
+    public int getRowCount()
+    {
+        return missionList.size();
+    }
+ 
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex)
+    {
+        Mission row = missionList.get(rowIndex);
+        if(0 == columnIndex) {
+            return row.getIdMission();
+        }
+        else if(1 == columnIndex) {
+            return row.getMissionName();
+        }
+        else if(2 == columnIndex) {
+            return row.getStartDate();
+        }
+        else if(3 == columnIndex) {
+            return row.getMissionDuration();
+        }
+        else if(4 == columnIndex) {
+            return row.getEtat();
+        }
+        return null;
+    }
+}
